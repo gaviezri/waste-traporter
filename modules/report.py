16fa9d2d1e686410d5da552c531bc043beb23290
gaviezri <g.aviezri@gmail.com>
2024-06-13 16:10:52 +0100

    wip

--- modules/report.py
+++ modules/report.py
@@ -1,5 +1,7 @@
 from pandas import DataFrame
 from openpyxl import Workbook
+from openpyxl.utils.dataframe import dataframe_to_rows
+from openpyxl.worksheet.table import Table, TableStyleInfo
 from typing import Dict, List
 from datetime import datetime
 from modules.types import WasteType, WeighingEntry
@@ -12,20 +14,27 @@ class ReportManager:
     DATE = "Date"
     WEIGHT = "Weight"
 
+    def __init__(self):
+        self.workbook = None
+
     def create_report(self, weighing_entries: Dict[WasteType ,List[WeighingEntry]]) -> str:
         now = datetime.now()
         report_path = PROJECT_ROOT / f"{now.year}-{now.month}.xlsx" 
 
         type_to_df = {}
-
-        for _type in [WasteType.WASTE, WasteType.PMD, WasteType.PAPER]:
-           type_to_df[_type] = self.__create_df_from_entries(weighing_entries[_type])
-
-        workbook 
-
-        summary_df = self.__create_summary_df(weighing_entries)
         
-        for _type, df 
+        for _type in [WasteType.WASTE, WasteType.PMD, WasteType.PAPER]:
+           type_to_df[_type] = self.__create_df_from_entries(weighing_entries[_type.value])
+
+        self.workbook = Workbook(write_only=True)
+
+        for _type, df in type_to_df.items():
+            self.__add_sheet(df, _type.value)
+        
+        summary_df = self.__create_summary_df(weighing_entries)
+        self.__add_sheet(summary_df, "Summary")
+
+        self.workbook.save(report_path)
 
         return report_path
 
@@ -42,11 +51,22 @@ class ReportManager:
         df = DataFrame(columns=[self.DATE, self.WEIGHT])
 
         for entry in sorted(entries, key=lambda entry: (entry.date_recorded, entry.time_recorded)):
-            datetime_str = entry.date_recorded.strftime("%Y-%m-%d ") + entry.time_recorded.strftime("%H:%M:%S")
+            datetime_str = entry.date_recorded.strftime("[%d] @ ") + entry.time_recorded.strftime("%H:%M")
             df.loc[len(df)] = {self.DATE: datetime_str, self.WEIGHT: entry.weight}
         
         return df
-        
+    
+    def __add_sheet(self, df, sheet_name: str):
+        sheet = self.workbook.create_sheet(title=sheet_name)
+
+        for i, row in enumerate(dataframe_to_rows(df, index=True, header=True)):
+            if i != 1:
+                sheet.append(row)
+
+        tab = Table(displayName=sheet_name, ref=f"A1:{chr(65 + len(df.columns))}{len(df) + 1}")
+        tab.tableStyleInfo = TableStyleInfo(name="TableStyleMedium9", showFirstColumn=False,
+                                            showLastColumn=False, showRowStripes=True, showColumnStripes=True)
+        sheet.add_table(tab)
 
 # TEST
 def test():
